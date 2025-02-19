c6c58767404542e00f6259039d91c30e8f43d484
kevinotto <ottokoit@gmail.com>
2025-02-19 09:57:03 +0200

    Has everything but no menu and music

--- TPT ball game 2025 Kevin OK/Assets/Scripts/PlayerController.cs
+++ TPT ball game 2025 Kevin OK/Assets/Scripts/PlayerController.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
     public float Speed;
-    public Text ScoreText;
-    public Text WinText;
+    public TMP_Text ScoreText;
+    public TMP_Text WinText;
     public GameObject Gate;
     private Rigidbody rb;
     public int Score;
@@ -16,7 +18,7 @@ public class PlayerController : MonoBehaviour
     void SetScoreText()
     {
         ScoreText.text = "Score: " + Score.ToString();
-        if(Score >= 5) 
+        if(Score == 10) 
         {
             WinText.text = "You won! Press R to restart or ESC to quit";
         }
@@ -45,6 +47,32 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.R)) 
         {
             Application.LoadLevel(Application.loadedLevel);
+        }
+
+        //Quit game
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+            Score += 1;
+            if(Score >= 5) 
+            {
+                Gate.gameObject.SetActive(false);
+            }
+            SetScoreText();
+
+        }
+
+        if (other.gameObject.CompareTag("Danger"))
+        {
+           Application.LoadLevel(Application.loadedLevel);
         }
     }
 }
